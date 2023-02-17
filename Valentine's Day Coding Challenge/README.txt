d2ace46f1f4364e3e62560f7eecebadb96a40730
anlu13 <71241994+anlu13@users.noreply.github.com>
2023-02-16 22:02:45 -0500

    Final Commit

---
+++ Valentine's Day Coding Challenge/README.txt
@@ -0,0 +1,17 @@
+Name: Anson L.
+Date: 2/16/2023
+
+Function:
+- This program displays "♥ML ACM" in ASCII text, outputted to the command line.
+
+Purpose:
+- This project was created as an entry submission to the UML ACM
+Valentine's Day Hackathon competition, which sought to design a Valentine's Day
+Themed Program.
+
+Program Execution:
+- To compile this program, enter "g++ -o VDayHeart VDayHeart.cpp" in the
+terminal command line at the correct directory. To execute the program, follow
+up the compile command with "./VDayHeart".
+- To run this program using the included Makefile, simply enter "make".to
+compile and execute the program
--- Valentine's Day Coding Challenge/VDayHeart.cpp
+++ Valentine's Day Coding Challenge/VDayHeart.cpp
@@ -1,24 +1,3 @@
-/*
-Name: Anson Lu
-Date: 2/16/2023
-
-Function:
-- This program displays "♥ML ACM" in ASCII text, outputted to the command line.
-
-Purpose:
-- This project was created as an entry submission to the UML ACM
-Valentine's Day Hackathon competition, which sought to design a Valentine's Day
-Themed Program.
-
-Program Execution:
-- To compile this program, enter "g++ -o VDayHeart VDayHeart.cpp" in the
-terminal command line at the correct directory. To execute the program, follow
-up the compile command with "./VDayHeart".
-- To run this program using the included Makefile, simply enter "make".to
-compile and execute the program
-
-*/
-
 #include <iostream>
 
 // function draws the shape of the top half of the heart (ie the 2 round
